edf23084c96c5200121d46356e56ca763126afa7
Yogesh Kushwah <YogeshKushwah775@gmail.com>
2025-02-20 01:50:01 +0530

    [Yogesh Kushwah] Add.  Create a Custom Annotation and Use It with testing

---
+++ Day06/src/annotation/test/customannotationstest/AnnotationExampleTest.java
@@ -0,0 +1,29 @@
+package test.annoations.customannotationstest;
+
+
+import org.example.com.annotations.customannotation.TaskInfo;
+import org.example.com.annotations.customannotation.TaskManager;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.lang.reflect.Method;
+import java.util.SortedMap;
+
+class AnnotationExampleTest {
+
+    @Test
+    void testTaskInfoAnnotation() throws Exception {
+        // Get the method reference
+        Method method = TaskManager.class.getMethod("completeTask");
+
+        // Check if annotation is present
+        assertTrue(method.isAnnotationPresent(TaskInfo.class));
+
+        // Get the annotation
+        TaskInfo taskInfo = method.getAnnotation(TaskInfo.class);
+
+        // Validate annotation values
+        assertEquals("High", taskInfo.priority());
+        assertEquals("Yogesh", taskInfo.assignedTo());
+        System.out.println("Test cases passed");
+    }
+}
