021182889dcac5bb3c7929bdf89b0aa021224054
Yogesh Kushwah <YogeshKushwah775@gmail.com>
2025-02-20 01:46:25 +0530

    [Yogesh Kushwah] Add.  Use @Deprecated to Mark an Old Method with testing

---
+++ Day06/src/annotation/test/deprecatedtest/DeprecatedExampleTest.java
@@ -0,0 +1,24 @@
+package test.annoations.deprecatedtest;
+
+
+
+import org.example.com.annotations.deprecated.LegacyAPI;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DeprecatedExampleTest {
+
+    @Test
+    public void testOfDeprecatedMethod() {
+	   // Create an object to make use of the deprecated method
+	   LegacyAPI object  = new LegacyAPI();
+
+	   // Call the deprecated method
+	   String actualResult = object.oldFeature();
+
+	   // Assert that the method returns the expected result
+	   assertTrue(actualResult.equals("Old Feature"));
+
+    }
+}
