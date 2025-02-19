47e9df560a57ddfc7f9c9e0d91534a0e7f77fcc1
Yogesh Kushwah <YogeshKushwah775@gmail.com>
2025-02-20 01:44:19 +0530

    [Yogesh Kushwah] Add.  Use @Override Correctly with testing

---
+++ Day06/src/annotation/test/overridetest/DogTest.java
@@ -0,0 +1,27 @@
+package test.annoations.overridetest;
+
+
+
+import org.example.com.annotations.override.Animal;
+import org.example.com.annotations.override.Dog;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class DogTest {
+    @Test
+   public void testDogMakeSound() {
+
+        // Creating an instance of Dog and calling makeSound()
+        Animal myDog = new Dog();
+
+        // Expected output
+        String expectedOutput = "Bark! Bark!\n";
+
+        // Comparing expected and actual output
+        assertTrue(expectedOutput.equals(myDog.makeSound()), "Dog's makeSound() should print 'Bark! Bark!'");
+    }
+}
