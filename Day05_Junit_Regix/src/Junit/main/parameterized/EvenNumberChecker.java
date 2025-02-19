9fa0482f0785837a3e8e23139313846bb3d3b9c0
Yogesh Kushwah <YogeshKushwah775@gmail.com>
2025-02-20 00:49:56 +0530

    [Yogesh Kushwah] Add. Testing Parameterized Tests

---
+++ Day05_Junit_Regix/src/Junit/main/parameterized/EvenNumberChecker.java
@@ -0,0 +1,8 @@
+package org.example.com.junit.parameterized;
+
+public class EvenNumberChecker {
+    //  Create a method to Checks whether a number is even.
+    public boolean isEven(int number) {
+        return (number& 1) == 0;
+    }
+}
