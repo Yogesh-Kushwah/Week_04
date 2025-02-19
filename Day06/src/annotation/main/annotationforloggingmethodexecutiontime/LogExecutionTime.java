da78d898eab4dac22d2069db800858d7e9d6b2de
Yogesh Kushwah <YogeshKushwah775@gmail.com>
2025-02-20 01:58:28 +0530

    [Yogesh Kushwah] Add.  Create an Annotation for Logging Method Execution Time with testing

---
+++ Day06/src/annotation/main/annotationforloggingmethodexecutiontime/LogExecutionTime.java
@@ -0,0 +1,12 @@
+package org.example.com.annotations.annotationforloggingmethodexecutiontime;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+// Defining the annotation @LogExecutionTime
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface LogExecutionTime {
+}
